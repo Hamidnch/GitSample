cb7d99c325bb640d2d80a45e977c2cd09a6cc6e5
hamidnch <hamidnch2007@gmail.com>
2021-09-23 15:30:00 +0330

    Move Button

--- GitSample/Form1.Designer.cs
+++ GitSample/Form1.Designer.cs
@@ -35,9 +35,9 @@ namespace GitSample
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(329, 248);
+            this.button1.Location = new System.Drawing.Point(186, 133);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(119, 23);
             this.button1.TabIndex = 0;
             this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;

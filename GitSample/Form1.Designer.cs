0e954faa8958111bb3908b49670f475d05857ec9
hamidnch <hamidnch2007@gmail.com>
2021-09-28 13:59:53 +0330

    Form1 Changed

--- GitSample/Form1.Designer.cs
+++ GitSample/Form1.Designer.cs
@@ -35,7 +35,7 @@ namespace GitSample
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(186, 133);
+            this.button1.Location = new System.Drawing.Point(293, 263);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(119, 23);
             this.button1.TabIndex = 0;

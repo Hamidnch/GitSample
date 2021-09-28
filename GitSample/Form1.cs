2498ba0bd3f45146d394de221d2d54fbfac27967
hamidnch <hamidnch2007@gmail.com>
2021-09-28 14:36:06 +0330

    Revert "Messagebox.Show"
    This reverts commit a431ba9fbeb932a7ae097f29dc8d1d3e594de8f0.

--- GitSample/Form1.cs
+++ GitSample/Form1.cs
@@ -1,4 +1,11 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace GitSample
@@ -10,9 +17,5 @@ namespace GitSample
             InitializeComponent();
         }
 
-        private void button1_Click(object sender, EventArgs e)
-        {
-            MessageBox.Show("Hello world!");
-        }
     }
 }
